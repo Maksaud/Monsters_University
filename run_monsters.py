6d68f397938fbc10b920d08ce4d34d982ae849e3
Maksaud <maksaud@hotmail.com>
2020-03-10 09:59:22 +0000

    added a control flow to make it more user freindly

--- run_monsters.py
+++ run_monsters.py
@@ -4,28 +4,55 @@ from monsters_class import *
 from monster_workshops import *
 
 # Creating a student
-student1 = Student('Maksaud', 'Ahmed', 1)
+# student1 = Student('Maksaud', 'Ahmed', 1)
+#
+# list_of_student = []
+#
+# # A list of students
+# list_of_student.append(student1)
+#
+# # Creating workshops
+# workshop1 = MonsterWorkshop('Maths', 'mr man')
+# workshop2 = MonsterWorkshop('English', 'mr woman')
+#
+# # Listing workshops
+# list_of_workshop = []
+#  # Putting each set of workshops into the list.
+# list_of_workshop.append(workshop1.set_workshops()) # subject
+# list_of_workshop.append(workshop2.set_workshops())
+#
+# print(list_of_workshop)
+#
+# # Creating a list of skills
+# student1.skills.append('stealth')
+# student1.skills.append('streangth')
+#
+#Printing all skiils of students
+# print(student1.skills)
 
-list_of_student = []
-
-# A list of students
-list_of_student.append(student1)
-
-# Creating workshops
-workshop1 = MonsterWorkshop('Maths', 'mr man')
-workshop2 = MonsterWorkshop('English', 'mr woman')
-
-# Listing workshops
-list_of_workshop = []
- # Putting each set of workshops into the list.
-list_of_workshop.append(workshop1.set_workshops()) # subject
-list_of_workshop.append(workshop2.set_workshops())
-
-print(list_of_workshop)
-
-# Creating a list of skills
-student1.skills.append('stealth')
-student1.skills.append('streangth')
-
-# Printing all skiils of students
-print(student1.skills)
+user_input = ""
+database = {}
+id = 0
+while user_input != 'quit':
+    user_input = input("If you want to add a student or teacher type: add, if you want to add a subject type: subject, if you want to add a skill: skill, if you want to quit: quit ")
+    if user_input == 'add':
+        student_or_teach = input('input a student or teacher ')
+        if student_or_teach == 'student':
+            database[id] = Student(input('Enter the students first name '), input('Enter students last name '), id)
+            print(database[id].f_name, database[id].l_name + 'is added')
+            id += 1
+        elif student_or_teach == 'teacher':
+            database[id] = Teacher(input('Enter the teachers first name '), input('Enter the teachers last name '), id)
+            print(database[id].f_name, database[id].l_name + 'is added')
+            id += 1
+        else:
+            user_input = 'quit'
+    elif user_input == 'subject':
+        workshop = MonsterWorkshops(input('Enter the subject name '), input('Enter teacher name '))
+        database["workshops"] = {workshop.f_name:workshop.teacher}
+    elif user_input == 'skill':
+        skill_input = input('Enter a skill ')
+        which_id = input("which id do you want to change ")
+        which_id.skills.append(skill_input)
+    else:
+        user_input='quit'
--- teacher_class.py
+++ teacher_class.py
@@ -1,6 +1,6 @@
 from monsters_class import *
 
 class Teacher(Monsters):
-    def __init__(self, f_name, l_name, skills, staff_id):
-        super().__init__(f_name, l_name, skills)
+    def __init__(self, f_name, l_name, staff_id):
+        super().__init__(f_name, l_name)
         self.staff_id = staff_id
