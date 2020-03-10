bc7ad89c12e0015a98aad5346062e5b0f979bf51
Maksaud <maksaud@hotmail.com>
2020-03-10 11:55:47 +0000

    added a database and edited code to work around the database

--- run_monsters.py
+++ run_monsters.py
@@ -31,28 +31,30 @@ from monster_workshops import *
 # print(student1.skills)
 
 user_input = ""
-database = {}
+database = {'Workshops':{}}
 id = 0
 while user_input != 'quit':
     user_input = input("If you want to add a student or teacher type: add, if you want to add a subject type: subject, if you want to add a skill: skill, if you want to quit: quit ")
     if user_input == 'add':
         student_or_teach = input('input a student or teacher ')
         if student_or_teach == 'student':
-            database[id] = Student(input('Enter the students first name '), input('Enter students last name '), id)
-            print(database[id].f_name, database[id].l_name + 'is added')
+            database[str(id)] = Student(input('Enter the students first name '), input('Enter students last name '), id)
+            print(database[str(id)].f_name, database[str(id)].l_name + 'is added and id is: ' + str(database[str(id)].student_id))
             id += 1
         elif student_or_teach == 'teacher':
-            database[id] = Teacher(input('Enter the teachers first name '), input('Enter the teachers last name '), id)
-            print(database[id].f_name, database[id].l_name + 'is added')
+            database[str(id)] = Teacher(input('Enter the teachers first name '), input('Enter the teachers last name '), id)
+            print(database[str(id)].f_name, database[str(id)].l_name + 'is added and id is: ' + str(database[str(id)].student_id))
             id += 1
         else:
             user_input = 'quit'
     elif user_input == 'subject':
-        workshop = MonsterWorkshops(input('Enter the subject name '), input('Enter teacher name '))
-        database["workshops"] = {workshop.f_name:workshop.teacher}
+        workshop = MonsterWorkshop(input('Enter the subject name '), input('Enter teacher name '))
+        database["Workshops"][workshop.subject] = workshop.teacher
+        print(database["Workshops"])
     elif user_input == 'skill':
         skill_input = input('Enter a skill ')
-        which_id = input("which id do you want to change ")
-        which_id.skills.append(skill_input)
+        which_id = int(input("which id do you want to change "))
+        database[str(which_id)].skills.append(skill_input)
+        print(database[str(which_id)].f_name, database[str(which_id)].skills)
     else:
         user_input='quit'
